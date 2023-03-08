1c80a688390e917599b75fccf43e74f6e587ed44
Bespoke34 <62766082+Bespoke34@users.noreply.github.com>
2023-03-08 12:11:16 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const colorChangeButton = document.getElementById("color-change-button");
-
-colorChangeButton.addEventListener("click", function() {
-  colorChangeButton.classList.toggle("clicked");
-});
